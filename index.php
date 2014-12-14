3ca6caea6558fe83345e90f8bdd63efa99662f59
lokendrashukla <lok7.shukla7@gmail.com>
2014-12-14 13:57:43 +0530

    Issue Creator

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * 
+ * Create issues on github and bitbucket
+ * Example for github :- php index.php create-issue -u lokendrashukla https://github.com/lokendrashukla/issueCreator "Issue 1" "This is a sample issue."
+ * Example for bitbucket :- php index.php create-issue -u lokendrashukla https://bitbucket.org/lokendrashukla/issueCreator "Issue 1" "This is a sample issue."
+ * @author Lokendra Shukla
+ */
+define('APPLICATION_PATH', dirname(__FILE__));
+
+require APPLICATION_PATH . DIRECTORY_SEPARATOR . 'Library'.DIRECTORY_SEPARATOR.'IssueManager.php';
+
+try {
+
+    if (PHP_SAPI != 'cli') {
+        throw new \Exception('This script will run from the command line!');
+    }
+
+    // unset the first argument which conatains file name
+    array_shift($argv);
+
+    // create the issue object and call the execute
+    $issue = new IssueCreator\IssueManager($argv);
+    echo $issue->execute();
+    
+} catch (Exception $e) {
+    echo "Error : " . $e->getMessage() . "\n";
+}
